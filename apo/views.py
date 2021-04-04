c1e82895a3401e8a04957e29b7155c91644887b9
RodrighoNS <rnavarro@lemontech.com>
2021-04-03 22:38:03 -0300

    Add APO views

--- apo/views.py
+++ apo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
+from .models import Apo
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class ApoViewSet(ModelViewSet):
+    queryset = Apo.objects.all()
+    serializer_class = ApoSerializer
+    permission_classes = [AllowAny]
