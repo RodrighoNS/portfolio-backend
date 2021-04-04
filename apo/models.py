6c4bb74781085f6d7e598f3db6328a9a693cea9b
RodrighoNS <rnavarro@lemontech.com>
2021-04-03 22:37:19 -0300

    Add APO model

--- apo/models.py
+++ apo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Apo(models.Model):
+  copyright = models.CharField(max_length=255)
+  date = models.CharField(max_length=255)
+  explanation = models.CharField(max_length=255)
+  hdurl = models.CharField(max_length=255)
+  media_type = models.CharField(max_length=255)
+  service_version = models.CharField(max_length=255)
+  title = models.CharField(max_length=255)
+  url = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+  return self.title
